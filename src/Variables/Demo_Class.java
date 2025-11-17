b15b475894794f360370da4d81453519faa7dd0e
Shraddha <shraddha.dangiste1102@gmail.com>
2025-11-17 13:25:41 +0530

    A and B values printed

--- src/Variables/Demo_Class.java
+++ src/Variables/Demo_Class.java
@@ -1,4 +1,16 @@
 package Variables;
 
 public class Demo_Class {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int a = 4;
+        int b = 44;
+
+        System.out.println("A value is "+a);
+        System.out.println("B value is "+b);
+        System.out.println("B value is "+b);
+    }
 }
